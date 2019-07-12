d7d60afbd47d9fa5d3a6e497ecf3210f17439bd9
Dan <danphi@gmail.com>
2019-07-11 18:46:16 -0700

    add random number

---
+++ classes/nodes/number/RandomNumberProperties.jsx
@@ -0,0 +1,35 @@
+import Properties from '../Properties.js';
+
+export default class RandomNumberProperties extends Properties {
+
+  constructor(props) {
+    super(props);
+
+    this.run = this.run.bind(this);
+  }
+
+
+  run(event) {
+    this.props.node.run();
+  }
+
+
+  render() {
+    return (
+      <div>
+        <div className="propertiesTitle">Random Number</div>
+
+        <div style={{padding:'10px'}}>
+          Random number between 0 and 1.<br/>
+          <br/>
+          <button onClick={(event) => {this.run(event)}}>Regenerate</button>
+          <br/><br/>
+          When the regenerate input changes this node will generate a new random number.
+          <br/>
+          <br/>
+          Regenerate input is optional.
+        </div>
+      </div>
+    )
+  }
+}
