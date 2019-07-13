92d8faf4ed319c28393465e7d20c3ee99ff5c10d
Dan <danphi@gmail.com>
2019-07-12 19:57:01 -0700

    Make all nodes run sync if inside a loop.

--- classes/nodes/resize/Resize.js
+++ classes/nodes/resize/Resize.js
@@ -28,24 +28,32 @@ export default class Resize extends NodeImage {
     if (this.inputs[0].image) {
       this.bg.classList.add('running');
       this.runTimer = Date.now();
-      Jimp.read(this.inputs[0].image).then(image => {
-        let resizeX = this.resizeX;
-        let resizeY = this.resizeY;
 
-        if (this.inputs[1].number != null) {
-          resizeX = this.inputs[1].number;
-        }
+      let resizeX = this.resizeX;
+      let resizeY = this.resizeY;
 
-        if (this.inputs[2].number != null) {
-          resizeY = this.inputs[2].number;
-        }
+      if (this.inputs[1].number != null) {
+        resizeX = this.inputs[1].number;
+      }
 
+      if (this.inputs[2].number != null) {
+        resizeY = this.inputs[2].number;
+      }
 
-        image.resize(resizeX, resizeY, this.mode, (error, image) => {
-          this.image = image;
-          super.run(inputThatTriggered);
-        });
-      })
+      if (this.isInsideALoop) {
+        let image = this.inputs[0].image.clone();
+        image.resize(resizeX, resizeY, this.mode);
+        this.image = image;
+        super.run(inputThatTriggered);
+      } else {
+        Jimp.read(this.inputs[0].image).then(image => {
+          image.resize(resizeX, resizeY, this.mode, (error, image) => {
+            this.image = image;
+            super.run(inputThatTriggered);
+          });
+        })
+      }
+
     } else {
       this.runTimer = Date.now();
       this.image = null;
