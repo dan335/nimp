0d49300355258ce2ccf2aee32de1e2e97044258f
Dan <danphi@gmail.com>
2019-07-12 21:14:18 -0700

    added round

---
+++ classes/nodes/number/Round.js
@@ -0,0 +1,39 @@
+import NodeNumber from '../NodeNumber.js';
+import RoundProperties from './RoundProperties.jsx';
+import RoundInputNumberA from './RoundInputNumberA.js';
+import OutputNumber from '../OutputNumber.js';
+
+export default class Round extends NodeNumber {
+  constructor(graph, x, y) {
+    super(graph, x, y, 'Round', RoundProperties);
+
+    this.inputs = [
+      new RoundInputNumberA(this, 0, 'a'),
+    ];
+    this.outputs = [
+      new OutputNumber(this, 0, 'Result')
+    ];
+
+    this.a = null;
+  }
+
+
+  run(inputThatTriggered) {
+    if (this.inputs[0].number == null) {
+      this.a = null;
+    } else {
+      this.a = this.inputs[0].number;
+    }
+
+    if (this.a != null && !isNaN(this.a)) {
+      this.bg.classList.add('running');
+      this.runTimer = Date.now();
+      this.number = Math.round(this.a);
+      super.run(inputThatTriggered);
+    } else {
+      this.runTimer = Date.now();
+      this.number = null;
+      super.run(inputThatTriggered);
+    }
+  }
+}
---
+++ classes/nodes/number/RoundProperties.jsx
@@ -0,0 +1,35 @@
+import Properties from '../Properties.js';
+
+export default class RoundProperties extends Properties {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hasA: props.node.inputs[0].parent ? true : false,
+    }
+  }
+
+
+  renderResult() {
+    if (this.state.hasA) {
+      return (
+        <div>
+          {this.props.node.a}= {Math.round(this.props.node.a)}
+        </div>
+      )
+    }
+  }
+
+
+  render() {
+    return (
+      <div>
+        <div className="propertiesTitle">Round a Number</div>
+        <div style={{padding:'10px'}}>
+          {this.renderResult()}
+        </div>
+      </div>
+    )
+  }
+}
