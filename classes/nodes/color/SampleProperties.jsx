7e28d342443e98db2fb77e4a5dd7db4ffd474158
Dan <danphi@gmail.com>
2019-07-16 22:00:15 -0700

    add sample node

---
+++ classes/nodes/color/SampleProperties.jsx
@@ -0,0 +1,70 @@
+import Properties from '../Properties.js';
+
+export default class SampleProperties extends Properties {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hasXInput: props.node.inputs[1].parent ? true : false,
+      hasYInput: props.node.inputs[2].parent ? true : false,
+    }
+
+    this.xChange = this.xChange.bind(this);
+    this.yChange = this.yChange.bind(this);
+  }
+
+
+  xChange(event) {
+    const elm = document.getElementById('xInput');
+    this.props.node.xValue = Number(elm.value);
+    this.props.node.run(null);
+  }
+
+  yChange(event) {
+    const elm = document.getElementById('yInput');
+    this.props.node.yValue = Number(elm.value);
+    this.props.node.run(null);
+  }
+
+
+
+  renderX() {
+    if (!this.state.hasXInput) {
+      return (
+        <div>
+          X &nbsp;
+          <input id="xInput" type="number" min="0" defaultValue={this.props.node.xValue} onChange={(event) => {this.xChange(event);}} />
+          <br/>
+        </div>
+      )
+    }
+  }
+
+  renderY() {
+    if (!this.state.hasYInput) {
+      return (
+        <div>
+          Y &nbsp;
+          <input id="yInput" type="number" min="0" defaultValue={this.props.node.yValue} onChange={(event) => {this.yChange(event);}} />
+          <br/>
+        </div>
+      )
+    }
+  }
+
+
+  render() {
+    return (
+      <div>
+        <div className="propertiesTitle">Sample</div>
+        <div style={{padding:'10px'}}>
+          Get color of image at pixel x,y.<br/>
+          <br/>
+          {this.renderX()}
+          {this.renderY()}
+        </div>
+      </div>
+    )
+  }
+}
---
+++ classes/nodes/inputs/InputNumberX.js
@@ -0,0 +1,22 @@
+import InputNumber from '../InputNumber.js';
+
+
+export default class InputNumberX extends InputNumber {
+  constructor(node, index, name) {
+    super(node, index, name);
+  }
+
+
+  connectionMade() {
+    if (this.node.bg.classList.contains('selected')) {
+      this.node.propertiesComponentInstance.setState({hasXInput:true});
+    }
+  }
+
+
+  connectionRemoved() {
+    if (this.node.bg.classList.contains('selected')) {
+      this.node.propertiesComponentInstance.setState({hasXInput:false});
+    }
+  }
+}
