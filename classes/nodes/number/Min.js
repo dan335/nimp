c6998e06b4b23c75c66a8e40d8837836eabd5f01
Dan <danphi@gmail.com>
2019-07-18 08:50:43 -0700

    min and max number nodes

---
+++ classes/nodes/number/Min.js
@@ -0,0 +1,58 @@
+import NodeNumber from '../NodeNumber.js';
+import MinProperties from './MinProperties.jsx';
+import InputNumberA from '../inputs/InputNumberA.js';
+import InputNumberB from '../inputs/InputNumberB.js';
+import OutputNumber from '../OutputNumber.js';
+
+export default class Min extends NodeNumber {
+  constructor(className, graph, x, y, settings) {
+    super(className, graph, x, y, 'Min', MinProperties);
+
+    this.inputs = [
+      new InputNumberA(this, 0, 'a'),
+      new InputNumberB(this, 1, 'b'),
+    ];
+    this.outputs = [
+      new OutputNumber(this, 0, 'Result')
+    ];
+
+    this.a = typeof settings.a !== 'undefined' ? settings.a : null;
+    this.b = typeof settings.b !== 'undefined' ? settings.b : null;
+  }
+
+
+  toJson() {
+    let json = super.toJson();
+
+    json.settings.a = this.a;
+    json.settings.b = this.b;
+
+    return json;
+  }
+
+
+  run(inputThatTriggered) {
+    if (this.inputs[0].number == null) {
+      this.a = null;
+    } else {
+      this.a = this.inputs[0].number;
+    }
+
+    if (this.inputs[1].number == null) {
+      this.b = null;
+    } else {
+      this.b = this.inputs[1].number;
+    }
+
+    if (this.a != null && this.b != null && !isNaN(this.a) && !isNaN(this.b)) {
+      this.bg.classList.add('running');
+      this.runTimer = Date.now();
+      this.number = Math.min(this.a, this.b);
+      super.run(inputThatTriggered);
+    } else {
+      this.runTimer = Date.now();
+      this.number = null;
+      super.run(inputThatTriggered);
+    }
+  }
+}
---
+++ classes/nodes/number/MinProperties.jsx
@@ -0,0 +1,37 @@
+import Properties from '../Properties.js';
+
+export default class MinNumbersProperties extends Properties {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hasA: props.node.inputs[0].parent ? true : false,
+      hasB: props.node.inputs[1].parent ? true : false
+    }
+  }
+
+
+  renderResult() {
+    if (this.state.hasA && this.state.hasB) {
+      return (
+        <div>
+          min({this.props.node.a}, {this.props.node.b}) = {Math.min(this.props.node.a, this.props.node.b)}
+        </div>
+      )
+    }
+  }
+
+
+  render() {
+    return (
+      <div>
+        <div className="propertiesTitle">Minimum of Two Numbers</div>
+        <div style={{padding:'10px'}}>
+          {this.renderResult()}
+          {this.renderRun()}
+        </div>
+      </div>
+    )
+  }
+}
