e685256add868002bf04258dc9b7d45d62e597a5
Dan <danphi@gmail.com>
2019-07-18 23:53:31 -0700

    get alpha, get luminance, get brightness

---
+++ classes/nodes/color/GetLuminance.js
@@ -0,0 +1,43 @@
+import NodeNumber from '../NodeNumber.js';
+import GetLuminanceProperties from './GetLuminanceProperties.jsx';
+import OutputNumber from '../OutputNumber.js';
+const tinycolor = require("tinycolor2");
+import InputColor from '../InputColor.js';
+
+
+export default class GetLuminance extends NodeNumber {
+  constructor(className, graph, x, y, settings) {
+    super(className, graph, x, y, 'Get Luminance', GetLuminanceProperties);
+
+    this.inputs = [
+      new InputColor(this, 0, 'Input')
+    ];
+    this.outputs = [
+      new OutputNumber(this, 0, 'Luminance')
+    ];
+  }
+
+
+  toJson() {
+    let json = super.toJson();
+
+    return json;
+  }
+
+
+  run(inputThatTriggered) {
+    this.bg.classList.add('running');
+    this.runTimer = Date.now();
+
+    if (this.inputs[0].color) {
+
+      this.number = this.inputs[0].color.clone().getLuminance();
+
+    } else {
+
+      this.color = null
+    }
+
+    super.run(inputThatTriggered);
+  }
+}
