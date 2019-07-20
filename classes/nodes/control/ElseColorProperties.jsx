b409cfb80e5d690bd41a56a47656c31cbcc12b58
Dan <danphi@gmail.com>
2019-07-20 01:11:39 -0700

    number and color if else nodes

---
+++ classes/nodes/control/ElseColorProperties.jsx
@@ -0,0 +1,49 @@
+import Properties from '../Properties.js';
+
+export default class ElseProperties extends Properties {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hasAInput: props.node.inputs[2].parent ? true : false
+    }
+
+    this.aChange = this.aChange.bind(this);
+  }
+
+
+  aChange(event) {
+    const elm = document.getElementById('aInput');
+    this.props.node.a = Number(elm.value);
+    this.props.node.run(null);
+  }
+
+
+  renderA() {
+    if (!this.state.hasAInput) {
+      return (
+        <div>
+          Test Number<br/>
+          <input id="aInput" type="number" defaultValue={this.props.node.a} onChange={(event) => {this.aChange(event);}} />
+        </div>
+      )
+    }
+  }
+
+
+  render() {
+    return (
+      <div>
+        <div className="propertiesTitle">If Else Color</div>
+        <div style={{padding:'10px'}}>
+          {this.renderA()}
+
+          <br/><br/>
+          Outputs color A if test number is true.  Outputs color B if test number is false.  Number is considered false if it is 0 or -0.
+          {this.renderRun()}
+        </div>
+      </div>
+    )
+  }
+}
