4a65b8d587a429266b6c6f6f5de25dcf7ff6bc89
Dan <danphi@gmail.com>
2019-07-20 01:11:39 -0700

    number and color if else nodes

---
+++ classes/nodes/control/ElseColor.js
@@ -0,0 +1,65 @@
+import NodeColor from '../NodeColor.js';
+import ElseColorProperties from './ElseColorProperties.jsx';
+import OutputColor from '../OutputColor.js';
+import InputColor from '../InputColor.js';
+import ElseInputNumberA from './ElseInputNumberA.js';
+const tinycolor = require("tinycolor2");
+
+
+export default class ElseColor extends NodeColor {
+  constructor(className, graph, x, y, settings) {
+    super(className, graph, x, y, 'Color If Else', ElseColorProperties);
+
+    this.inputs = [
+      new InputColor(this, 0, 'Input A'),
+      new InputColor(this, 1, 'Input B'),
+      new ElseInputNumberA(this, 2, 'Test')
+    ];
+    this.outputs = [
+      new OutputColor(this, 0, 'Output')
+    ];
+
+    this.a = typeof settings.a !== 'undefined' ? settings.a : 1;
+  }
+
+
+  toJson() {
+    let json = super.toJson();
+
+    json.settings.a = this.a;
+
+    return json;
+  }
+
+
+  run(inputThatTriggered) {
+    let a = this.a;
+
+    if (this.inputs[2].number != null) {
+      a = this.inputs[2].number;
+    }
+
+    if (this.inputs[0].color && this.inputs[1].color) {
+      this.bg.classList.add('running');
+      this.runTimer = Date.now();
+
+      this.color = tinycolor('#000');
+      if (a) {
+        if (this.inputs[0].color) {
+          this.color = this.inputs[0].color;
+        }
+      } else {
+        if (this.inputs[1].color) {
+          this.color = this.inputs[1].color;
+        }
+      }
+
+      super.run(inputThatTriggered);
+
+    } else {
+      this.runTimer = Date.now();
+      this.color = tinycolor('#000');
+      super.run(inputThatTriggered);
+    }
+  }
+}
---
+++ classes/nodes/control/ElseNumber.js
@@ -0,0 +1,64 @@
+import NodeNumber from '../NodeNumber.js';
+import ElseNumberProperties from './ElseNumberProperties.jsx';
+import OutputNumber from '../OutputNumber.js';
+import InputNumber from '../InputNumber.js';
+import ElseInputNumberA from './ElseInputNumberA.js';
+
+
+export default class ElseNumber extends NodeNumber {
+  constructor(className, graph, x, y, settings) {
+    super(className, graph, x, y, 'Number If Else', ElseNumberProperties);
+
+    this.inputs = [
+      new InputNumber(this, 0, 'Input A'),
+      new InputNumber(this, 1, 'Input B'),
+      new ElseInputNumberA(this, 2, 'Test')
+    ];
+    this.outputs = [
+      new OutputNumber(this, 0, 'Output')
+    ];
+
+    this.a = typeof settings.a !== 'undefined' ? settings.a : 1;
+  }
+
+
+  toJson() {
+    let json = super.toJson();
+
+    json.settings.a = this.a;
+
+    return json;
+  }
+
+
+  run(inputThatTriggered) {
+    let a = this.a;
+
+    if (this.inputs[2].number != null) {
+      a = this.inputs[2].number;
+    }
+
+    if (this.inputs[0].number && this.inputs[1].number) {
+      this.bg.classList.add('running');
+      this.runTimer = Date.now();
+
+      this.number = null;
+      if (a) {
+        if (this.inputs[0].number) {
+          this.number = this.inputs[0].number;
+        }
+      } else {
+        if (this.inputs[1].number) {
+          this.number = this.inputs[1].number;
+        }
+      }
+
+      super.run(inputThatTriggered);
+
+    } else {
+      this.runTimer = Date.now();
+      this.number = null;
+      super.run(inputThatTriggered);
+    }
+  }
+}
