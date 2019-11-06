2cc8977b081eb261c223b9e14c2d5e9a2ca8026f
Dan <danphi@gmail.com>
2019-11-05 21:59:01 -0800

    rename node name in ifs

--- classes/nodes/control/IfColorProperties.jsx
+++ classes/nodes/control/IfColorProperties.jsx
@@ -9,7 +9,7 @@ export default class IfProperties extends Properties {
       <div>
         <div className="propertiesTitle">Color If</div>
         <div style={{padding:'10px'}}>
-          <PropertiesInputNumber node={this.props.node} name="A" varName={'a'} input={null} min={0} max={1} step={1} />
+          <PropertiesInputNumber node={this.props.node} name="Test" varName={'a'} input={null} min={0} max={1} step={1} />
 
           <br/><br/>
           Outputs color if test number is true.  Does nothing if number is false.  Number is considered false if it is 0 or -0.
