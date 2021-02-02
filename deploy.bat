24cef0897f1a8dd9c4a8ac375c2475aec6b936f3
Dan <danphi@gmail.com>
2021-02-02 11:07:06 -0800

    deploy script

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
 docker login registry.gitlab.com -u danphi
 docker build -t registry.gitlab.com/danphi/nimp .
 docker push registry.gitlab.com/danphi/nimp
-ssh root@nimp.app "cd /shipyard/compose; docker-compose pull; docker-compose stop nimp; docker-compose rm -f nimp; docker-compose up -d"
+ssh root@nimp.app "cd /shipyard/compose; docker-compose pull; docker-compose up -d"
