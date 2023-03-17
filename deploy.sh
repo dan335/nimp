d61523312d66d1fc0101ef6777bcf1b9aab3e91d
Dan <danphi@gmail.com>
2023-03-16 22:26:21 -0400

    update packages

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 docker login registry.gitlab.com -u danphi
 docker build -t registry.gitlab.com/danphi/nimp --platform linux/amd64 .
 docker push registry.gitlab.com/danphi/nimp
+ssh root@nimp.app "cd /shipyard/compose; docker-compose pull; docker-compose up -d"
