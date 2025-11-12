a35406666ee34c9fc445f4bdcd30147e322c52d5
Dan <danphi@gmail.com>
2025-11-12 16:55:07 -0500

    asdf

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const path = require('path');
-const next = require('next');
-const bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const next = require("next");
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3000;
-const MongoClient = require('mongodb').MongoClient;
-console.log('MONGO_URL', process.env.MONGO_URL);
+const MongoClient = require("mongodb").MongoClient;
+console.log("MONGO_URL", process.env.MONGO_URL);
 const mongoClient = new MongoClient(process.env.MONGO_URL);
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== "production";
 const nextApp = next({ dev });
 const handle = nextApp.getRequestHandler();
-const session = require('express-session');
-const MongoStore = require('connect-mongodb-session')(session);
+const session = require("express-session");
+const MongoStore = require("connect-mongodb-session")(session);
 
 // const uri = process.env.MONGO_URL;
 // const options = {
@@ -23,9 +23,9 @@ const MongoStore = require('connect-mongodb-session')(session);
 //let mongoClient = new MongoClient(uri, options)
 //let clientPromise = mongoClient.connect()
 
-mongoClient.connect(error => {
+mongoClient.connect((error) => {
   if (error) {
-    console.log('Error connecting to mongodb.');
+    console.log("Error connecting to mongodb.");
     console.log(error);
     return;
   }
@@ -33,45 +33,48 @@ mongoClient.connect(error => {
   nextApp.prepare().then(() => {
     const expressApp = express();
 
-    expressApp.locals.db = mongoClient.db('nimp');
+    expressApp.locals.db = mongoClient.db("nimp");
 
-    expressApp.locals.db.createIndex('users', {email:1});
+    expressApp.locals.db.createIndex("users", { email: 1 });
 
-    
-
-    let store = new MongoStore({ clientPromise: clientPromise, collection: 'sessions' });
-
-    store.on('error', function(error) {
-      console.log('MongoStore Error', error);
+    let store = new MongoStore({
+      clientPromise: clientPromise,
+      collection: "sessions",
     });
 
-    expressApp.use(session({
-      secret: 'workhard',
-      store: store,
-      cookie: {maxAge: 1000*60*60*24*30},
-      resave: false,
-      saveUninitialized: false
-    }));
+    store.on("error", function (error) {
+      console.log("MongoStore Error", error);
+    });
 
-    expressApp.use(bodyParser.json({limit: '20mb', extended: true}));
-    expressApp.use(bodyParser.urlencoded({limit: '20mb', extended: true}));
+    expressApp.use(
+      session({
+        secret: "workhard",
+        store: store,
+        cookie: { maxAge: 1000 * 60 * 60 * 24 * 30 },
+        resave: false,
+        saveUninitialized: false,
+      }),
+    );
 
-    require('./api/savegraph.js')(expressApp);
-    require('./api/createaccount.js')(expressApp);
-    require('./api/login.js')(expressApp);
-    require('./api/logout.js')(expressApp);
-    require('./api/graph.js')(expressApp);
-    require('./api/graphs.js')(expressApp);
-    require('./api/deletegraph.js')(expressApp);
+    expressApp.use(bodyParser.json({ limit: "20mb", extended: true }));
+    expressApp.use(bodyParser.urlencoded({ limit: "20mb", extended: true }));
 
-    expressApp.get('*', (req,res) => {
-      return handle(req,res);
-    })
+    require("./api/savegraph.js")(expressApp);
+    require("./api/createaccount.js")(expressApp);
+    require("./api/login.js")(expressApp);
+    require("./api/logout.js")(expressApp);
+    require("./api/graph.js")(expressApp);
+    require("./api/graphs.js")(expressApp);
+    require("./api/deletegraph.js")(expressApp);
 
-    expressApp.listen(PORT, err => {
+    expressApp.get("*", (req, res) => {
+      return handle(req, res);
+    });
+
+    expressApp.listen(PORT, (err) => {
       if (err) throw err;
       console.log(`Nimp running on port ${PORT}`);
-    })
+    });
 
     // // delete graphs not viewed in the past 30 days
     // setInterval(() => {
@@ -79,5 +82,5 @@ mongoClient.connect(error => {
     //   const cutoff = new Date(new Date().setDate(new Date().getDate()-90));
     //   collection.deleteMany({viewedAt: {$lt:cutoff}});
     // }, 1000 * 60 * 60 * 6);
-  })
-})
+  });
+});
